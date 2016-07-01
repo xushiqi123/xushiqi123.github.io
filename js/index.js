ffb5583b82e66e91f0497a2365a44340908b29fc
unknown <>
2016-07-01 23:38:49 +0800

    'update'

--- js/index.js
+++ js/index.js
@@ -1,88 +1,44 @@
 /**
- * Created by ÊÀÆæ on 2016/6/26.
+ * Created by ï¿½ï¿½ï¿½ï¿½ on 2016/6/26.
  */
 
-//nav±êÌâµã»÷
+//navï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 $('nav>h1>a').click(function(e){
     e.preventDefault();
     var id = $(this).attr('href');
     $(id).addClass('active').siblings('.active').removeClass('active')
 })
-//ÈýÏîfocus
+//ï¿½ï¿½ï¿½ï¿½focus
 $('nav>ul>li>a').mouseover(function(){
     $(this).next().css('opacity','1')
         .css('top','-4em')
         .css('display','block').css('zIndex',1000);
 })
-//ÈýÏîblur
+//ï¿½ï¿½ï¿½ï¿½blur
 $('nav>ul>li>a').mouseout(function(){
     $(this).next().css('opacity','0')
         .css('top','0').css("display","block").css('zIndex',10);
     ;
 })
-//navÈýÏîµã»÷
+//navï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 $('nav>ul>li>a').click(function(e){
     e.preventDefault();
     var id = $(this).attr('href');
     $(id).addClass('active').siblings('.active').removeClass('active')
 })
-//sec0µÄÈýÏîµã»÷
+//sec0ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 $('#sec0>ul>li>a').click(function(e){
     e.preventDefault();
     var id=$(this).attr('href');
     $(id).addClass('active').siblings('.active').removeClass('active')
 })
-//sec2µÄhobbyµã»÷
-//$('#sec2>div>ul>li>a').click(function (e) {                   ???id
-$('#sec2>div>ul>li>a').click(function (e) {
-    e.preventDefault();
-    var class1=$(this).attr('href');
-    $(class1).addClass('active').siblings('.active').removeClass('active')
-})
-$('.btn1').click(function(){
-    $(this).parent().removeClass('active')
-})
 
-//hobby1µÄÍ¼Æ¬
-var hobby1=['a1.jpg','a2.jpg','a3.jpg','a4.jpg','a5.jpg','a6.jpg','a7.jpg','a8.jpg','a9.jpg'];
-//ÂÖ²¥°´Å¥before
-$('#carousel1>h6:first-child').click(function(){
-    //console.log($(this).next().attr('src'))
-    var i=parseInt($(this).next().attr('alt'));
-    if(i==1){i=9};
-    i--;
-    $(this).next().attr('alt',i);
-    i--;
-    $(this).next().attr('src','img/'+hobby1[i]);
-})
-//ÂÖ²¥°´Å¥after
-$('#carousel1>h6:last-child').click(function(){
-    //console.log($(this).next().attr('src'))
-    var i=parseInt($(this).prev().attr('alt'));
-    if(i==9){i=0};
-    $(this).prev().attr('src','img/'+hobby1[i]);
-    //console.log($(this).prev().attr('src'))
-    i++;
-    $(this).prev().attr('alt',i);
-    //console.log($(this).prev().attr('alt'))
-})
 
-//×ÔÖÆÍ¼Æ¬ÂÖ²¥
-var timer=setInterval(function(){
-    var j=$('#carousel1>img').attr('alt');
-    if(j>=9){j=0}
+//
+AOS.init({
+    easing: 'ease-in-back',
+    duration: 1000
+});
 
-        $('#carousel1>img').attr('src','img/'+hobby1[j]);
-        j++;
-        $('#carousel1>img').attr('alt',j);
-},5000);
-
-//ºóÍË¶¯Ì¬
-var direction=1;
-var timer2=setInterval(function(){
-    var lf=parseInt($('.hobby>h5').css('left'));
-    if(lf>30){direction=-1}
-    if(lf<0){direction=1}
-    lf+=10*direction;
-    $('.hobby>h5').css('left',lf)
-},160)
+//
+$('.my-accordion').accordion();
