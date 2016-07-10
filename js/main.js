c22455e21a79120ae287f9a3d6844b417de4d251
伍峰杰 <wufengjie@u51.com>
2016-07-11 01:50:32 +0800

    updaye

--- js/main.js
+++ js/main.js
@@ -16,19 +16,43 @@ Array.prototype.slice.call(document.querySelectorAll('.button'), 0).forEach(func
   });
 });
 
-initBt9();
+initBt2();
 
 
-// Button 9
-function initBt9() {
-  var bt = document.querySelectorAll('#component-9')[0];
-  var turb = document.querySelectorAll('#filter-ripple-2 feImage')[0];
-  var dm = document.querySelectorAll('#filter-ripple-2 feDisplacementMap')[0];
-  
-  bt.addEventListener('click', function(e) {
-    TweenLite.set(turb, { attr: { x: isFF ? e.offsetX : e.offsetX + 20, y: isFF ? e.offsetY : e.offsetY + 20, width: 0, height: 0 } });
-    TweenLite.to(turb, 5, { attr: { x: '-=300', y: '-=300', width: 600, height: 600 } });
-    TweenLite.fromTo(dm, 3, { attr: { scale: 30 } }, { attr: { scale: 0 } });
+
+// Button 2
+function initBt2() {
+  var bt = document.querySelectorAll('#component-2')[0];
+  var particleCount = 12;
+  var colors = ['#DE8AA0', '#8AAEDE', '#FFB300', '#60C7DA']
+
+  bt.addEventListener('click', function() {
+    var particles = [];
+    var tl = new TimelineLite();
+
+    tl.to(bt.querySelectorAll('.button__bg'), 0.6, { scaleX: 1.05 });
+    tl.to(bt.querySelectorAll('.button__bg'), 0.9, { scale: 1, ease: Elastic.easeOut.config(1.2, 0.4) }, 0.6);
+
+    for (var i = 0; i < particleCount; i++) {
+      particles.push(document.createElement('span'));
+      bt.appendChild(particles[i]);
+
+      particles[i].classList.add(i % 2 ? 'left' : 'right');
+
+      var dir = i % 2 ? '-' : '+';
+      var r = i % 2 ? getRandom(-1, 1)*i/2 : getRandom(-1, 1)*i;
+      var size = i < 2 ? 1 : getRandom(0.4, 0.8);
+      var tl = new TimelineLite({ onComplete: function(i) {
+        particles[i].parentNode.removeChild(particles[i]);
+        this.kill();
+      }, onCompleteParams: [i] });
+
+      tl.set(particles[i], { scale: size });
+      tl.to(particles[i], 0.6, { x: dir + 20, scaleX: 3, ease: SlowMo.ease.config(0.1, 0.7, false) });
+      tl.to(particles[i], 0.1, { scale: size, x: dir +'=25' }, '-=0.1');
+      if(i >= 2) tl.set(particles[i], { backgroundColor: colors[Math.round(getRandom(0, 3))] });
+      tl.to(particles[i], 0.6, { x: dir + getRandom(60, 100), y: r*10, scale: 0.1, ease: Power3.easeOut });
+      tl.to(particles[i], 0.2, { opacity: 0, ease: Power3.easeOut }, '-=0.2');
+    }
   });
 }
-
